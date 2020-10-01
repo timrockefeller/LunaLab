047a125e11699b9847d7ce5a78567cac49f1776a
kitekii <tim720719@163.com>
2020-10-01 23:31:48 +0800

    add: imgui Lib

--- src/Test/00_Environment/main.cpp
+++ src/Test/00_Environment/main.cpp
@@ -1,3 +1,11 @@
-int main (){
+#include <Common/Common.h>
+#include <Editor/Glfw.h>
+using namespace KTKR;
+using namespace LUNA;
+using namespace std;
+int main() {
+    Glfw::getInstance()->Init(1920, 1080, "Luna Test");
+    
+
     return 0;
 }
