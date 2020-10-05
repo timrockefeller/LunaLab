b2253aa303498b4f47ff0f090c5509ec29415ec9
kitekii <tim720719@163.com>
2020-10-05 11:58:05 +0800

    fix: fopen on multi sys

--- src/Basic/File.cpp
+++ src/Basic/File.cpp
@@ -4,26 +4,31 @@ using namespace KTKR;
 File::File(const string& fileName, Mode mode) {
     this->fileName = fileName;
     this->mode = mode;
+// https://stackoverflow.com/questions/32923947/fopen64-fseeko64-and-c-11
+#ifdef _WIN32
     fopen_s(&pF, fileName.c_str(), ModeToStr(mode).c_str());
+#else
+    pF = fopen64(fileName.c_str(), ModeToStr(mode).c_str());
+#endif
     if (pF == NULL)
         fprintf(stderr, "%s open error\n", fileName.c_str());
 }
 
 File::~File() {
-	if (pF != NULL) {
-		fclose(pF);
-		pF = NULL;
-	}
+    if (pF != NULL) {
+        fclose(pF);
+        pF = NULL;
+    }
 }
 
 string File::ReadLine() {
-	if (!Check(READ)) {
-		fprintf(stderr, "%s can't not read\n", fileName.c_str());
-		return "";
-	}
-	char buffer[1024] = "";
-	fgets(buffer, 1024, pF);
-	return buffer;
+    if (!Check(READ)) {
+        fprintf(stderr, "%s can't not read\n", fileName.c_str());
+        return "";
+    }
+    char buffer[1024] = "";
+    fgets(buffer, 1024, pF);
+    return buffer;
 }
 
 string File::ReadAll() {
@@ -49,7 +54,7 @@ bool File::IsValid() {
 }
 
 bool File::IsEnd() {
-	return feof(pF);
+    return feof(pF);
 }
 
 string File::ModeToStr(Mode mode) {
