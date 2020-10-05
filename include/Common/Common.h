82b6a74959027890c08f9b110c04578e12dee2fd
kitekii <tim720719@163.com>
2020-10-05 11:52:36 +0800

    add: common.node

--- include/Common/Common.h
+++ include/Common/Common.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <Common/Heap.h>
 #include <Common/File.h>
 #include <Common/Image.h>
 #include <Common/OpQueue.h>
 #include <Common/EventListener.h>
+#include <Common/Node.h>
--- include/Common/Heap.h
+++ include/Common/Heap.h
@@ -2,25 +2,28 @@
 #include <Common/Ptr.h>
 namespace KTKR {
 
-class Heap : public std::enable_shared_from_this<Heap> {
+class HeapObj : public std::enable_shared_from_this<HeapObj> {
     template <typename ImplT, typename... Args>
     friend const Ptr<ImplT> New(Args&&... args);
 
    protected:
-    template <typename T = Heap>
+    template <typename T = HeapObj>
     const Ptr<T> This() { return CastTo<T>(shared_from_this()); }
-    template <typename T = Heap>
+    template <typename T = HeapObj>
     const PtrC<T> This() const { return CastTo<const T>(shared_from_this()); }
-    template <typename T = Heap>
+    template <typename T = HeapObj>
     const WPtr<T> WThis() noexcept { return CastTo<T>(weak_from_this()); }
-    template <typename T = Heap>
+    template <typename T = HeapObj>
     const WPtrC<T> WThis() const noexcept { return CastTo<const T>(weak_from_this()); }
 
    protected:
+   /*
+    In order to use 4 pointer function defined previously, the initializer need be called after 'new' called. 
+   */
     virtual void LateInit() {}
 
    private:
-    static void Delete(Heap* obj) { delete obj; }
+    static void Delete(HeapObj* obj) { delete obj; }
 
    private:
     void* operator new(size_t size) {
@@ -34,16 +37,16 @@ class Heap : public std::enable_shared_from_this<Heap> {
     }
 
    private:
-    using std::enable_shared_from_this<Heap>::shared_from_this;
-    using std::enable_shared_from_this<Heap>::weak_from_this;
+    using std::enable_shared_from_this<HeapObj>::shared_from_this;
+    using std::enable_shared_from_this<HeapObj>::weak_from_this;
 };
 
 // https://stackoverflow.com/questions/48327250/in-function-parameter-pack
 // Combining forwarding reference with parameter pack, one can write easily generic proxy functions without performance loss in terms of lvalue/rvalue.
 template <typename ImplT, typename... Args>
 const Ptr<ImplT> New(Args&&... args) {
-    const auto pImplT = Ptr<ImplT>(new ImplT(std::forward<Args>(args)...), Heap::Delete);
-    auto pHeap = static_cast<Ptr<Heap>>(pImplT);
+    const auto pImplT = Ptr<ImplT>(new ImplT(std::forward<Args>(args)...), HeapObj::Delete);
+    auto pHeap = static_cast<Ptr<HeapObj>>(pImplT);
     pHeap->LateInit();
     return pImplT;
 }
