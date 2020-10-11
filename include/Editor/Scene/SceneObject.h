ba3226bb14681b457b68f8d16ad5e9ce7e2f2660
kitekii <tim720719@163.com>
2020-10-11 18:32:49 +0800

    add impl

--- include/Editor/Scene/SceneObject.h
+++ include/Editor/Scene/SceneObject.h
@@ -29,26 +29,49 @@ class SceneObject : public KTKR::Node<SceneObject> {
     template <typename T, typename = enable_if_is_component_t<T>>
     bool HaveComponent() const { return components.find(typeid(T)) != components.cend(); }
 
-    const std::vector<KTKR::Ptr<Component>> GetAllComponents() const;
+    const std::vector<KTKR::Ptr<Component>> GetAllComponents() const {
+        std::vector<KTKR::Ptr<Component>> rsl;
+        for (auto& cmp : components)
+            rsl.push_back(cmp.second);
+        return rsl;
+    }
 
     template <typename T, typename... Args, typename = enable_if_is_component_t<T>>
-    const KTKR::Ptr<T> AddComponent(Args&&... args){
-        if(HaveComponent<T>())
+    const KTKR::Ptr<T> AddComponent(Args&&... args) {
+        if (HaveComponent<T>())
             return nullptr;
         auto cmpt = T::New(This<SceneObject>(), std::forward<Args>(args)...);
         return cmpt;
     }
 
-    void AttachComponent(KTKR::Ptr<Component> component);
+    void AttachComponent(KTKR::Ptr<Component> component) {
+        auto target = components.find(typeid(*component));
+
+        if (target != components.end()) {
+            target->second->wSceneObject.reset();
+            component->wSceneObject = This<SceneObject>();
+            components[typeid(*component)] = component;
+        }
+    }
 
     template <typename T, typename = enable_if_is_component_t<T>>
-    bool DetachComponent(){
+    bool DetachComponent() {
         auto tarcmp = components.find(typeid(T));
-        if(tarcmp == components.end()) return false;
+        if (tarcmp == components.end())
+            return false;
         tarcmp->second->wSceneObject.reset();
         return true;
     }
-    bool DetachComponent(KTKR::Ptr<Component> component);
+    bool DetachComponent(KTKR::Ptr<Component> component) {
+        auto target = components.find(typeid(*component));
+        if (target == components.end())
+            return false;
+        if (target->second != component)
+            return false;
+        target->second->wSObj.reset();
+        components.erase(target);
+        return true;
+    }
 
     template <typename T, typename = enable_if_is_component_t<T>>
     const KTKR::Ptr<T> GetComponentInChildren();
