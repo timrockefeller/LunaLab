cfa97c36cd982d24dba81394691144aa0edcf92b
kitekii <tim720719@163.com>
2020-12-08 16:42:22 +0800

    update: singleton in mutex

--- include/Common/Singleton.h
+++ include/Common/Singleton.h
@@ -1,8 +1,9 @@
 #pragma once
+#include <mutex>
 #include <type_traits>
 namespace KTKR {
 template <typename T, typename = std::enable_if<std::is_class<T>::value>>
-class Singleton {
+class Singleton_Lagacy {
    public:
     static T* getInstance() noexcept {
         static T* instance = new T;
@@ -10,13 +11,40 @@ class Singleton {
     }
 
    protected:
+    Singleton_Lagacy() = default;
+    virtual ~Singleton_Lagacy() = default;
+
+   private:
+    Singleton_Lagacy(const Singleton_Lagacy&) = delete;
+    Singleton_Lagacy& operator=(const Singleton_Lagacy&) = delete;
+};
+
+template <typename T>
+class Singleton {
+   public:
+    static T& getInstanceObj() {
+        static std::once_flag flag;
+        std::call_once(flag, &Singleton::Get);
+        return Get();
+    }
+    static T* getInstance() {
+        static std::once_flag flag;
+        std::call_once(flag, &Singleton::Get);
+        return &Get();
+    }
+
+   protected:
     Singleton() = default;
     virtual ~Singleton() = default;
 
    private:
+    static T& Get() {
+        static T instance;
+        return instance;
+    }
     Singleton(const Singleton&) = delete;
-    Singleton& operator=(const Singleton&) = delete;
-
-};  // namespace KTKR
-
+    Singleton(Singleton&&) = delete;
+    const Singleton& operator=(const Singleton&) = delete;
+    const Singleton& operator=(Singleton&&) = delete;
+};
 }  // namespace KTKR
