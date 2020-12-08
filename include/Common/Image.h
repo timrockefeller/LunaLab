5582f9341c9e0a08dba1453a73be4305e605ba4a
kitekii <tim720719@163.com>
2020-12-08 14:05:17 +0800

    style

--- include/Common/Image.h
+++ include/Common/Image.h
@@ -4,20 +4,20 @@ namespace KTKR {
 typedef unsigned char uByte;
 class Image {
    public:
-    Image(const char* imagePath_,bool flip = false, int req_comp = 0);
+    Image(const char* imagePath_, bool flip = false, int req_comp = 0);
     Image();
     ~Image();
     int getWidth();
     int getHeight();
     int getChannels();
-    uByte * getData();
+    uByte* getData();
     bool isValid();
-    void Load(const char* imagePath_,bool flip,int req_comp);
-void Free();
+    void Load(const char* imagePath_, bool flip, int req_comp);
+    void Free();
+
    private:
-    
     std::string imagePath;
     uByte* data;
-    int width,height,channels;
+    int width, height, channels;
 };
 }  // namespace KTKR
