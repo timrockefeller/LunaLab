6f47ef8876069d121ac4d349d722aaed3e2572da
kitekii <tim720719@163.com>
2021-04-12 09:56:47 +0800

    style

--- src/OpenGL/Shader.cpp
+++ src/OpenGL/Shader.cpp
@@ -7,11 +7,12 @@
 using namespace std;
 using namespace LUNA;
 using namespace KTKR;
-Shader::Shader(const string& vertexShaderPath,
-               const string& fragmentShaderPath) {
+Shader::Shader(const string &vertexShaderPath,
+               const string &fragmentShaderPath)
+{
     File vsF(vertexShaderPath, File::Mode::READ);
     string vss = vsF.ReadAll();
-    const char* vertexShaderSource = vss.c_str();
+    const char *vertexShaderSource = vss.c_str();
 
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -23,7 +24,8 @@ Shader::Shader(const string& vertexShaderPath,
     char infoLog[512];
     // check valid
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
+    if (!success)
+    {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
         cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
              << infoLog << endl;
@@ -31,7 +33,7 @@ Shader::Shader(const string& vertexShaderPath,
 
     File fsF(fragmentShaderPath, File::Mode::READ);
     string fss = fsF.ReadAll();
-    const char* fragmentShaderSource = fss.c_str();
+    const char *fragmentShaderSource = fss.c_str();
 
     unsigned int fragmentShader;
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -40,7 +42,8 @@ Shader::Shader(const string& vertexShaderPath,
     glCompileShader(fragmentShader);
 
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
+    if (!success)
+    {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
                   << infoLog << std::endl;
@@ -52,58 +55,70 @@ Shader::Shader(const string& vertexShaderPath,
     glLinkProgram(ID);
     // check valid
     glGetProgramiv(ID, GL_LINK_STATUS, &success);
-    if (!success) {
+    if (!success)
+    {
         glGetProgramInfoLog(ID, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n"
                   << infoLog << std::endl;
     }
-
+    std::cout << "PASS::SHADER_LINK\n";
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 }
 
-unsigned int Shader::GetID() const {
+unsigned int Shader::GetID() const
+{
     return ID;
 }
 
-Shader::~Shader() {
+Shader::~Shader()
+{
     glDeleteProgram(ID);
 }
 
-void Shader::Use() const {
+void Shader::Use() const
+{
     if (isValid())
         glUseProgram(ID);
 }
 
-bool Shader::isValid() const {
+bool Shader::isValid() const
+{
     return glIsProgram(ID);
 }
 
-void Shader::setBool(const std::string& name, bool value) const {
+void Shader::setBool(const std::string &name, bool value) const
+{
     Use();
     glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
 }
-void Shader::setInt(const std::string& name, int value) const {
+void Shader::setInt(const std::string &name, int value) const
+{
     Use();
     glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
 }
-void Shader::setFloat(const std::string& name, float value) const {
+void Shader::setFloat(const std::string &name, float value) const
+{
     Use();
     glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 }
-void Shader::setVec3f(const std::string& name, float v1, float v2, float v3) {
+void Shader::setVec3f(const std::string &name, float v1, float v2, float v3)
+{
     Use();
     glUniform3f(glGetUniformLocation(ID, name.c_str()), v1, v2, v3);
 }
-void Shader::setVec3f(const std::string& name, glm::vec3 vec) {
+void Shader::setVec3f(const std::string &name, glm::vec3 vec)
+{
     Use();
     glUniform3f(glGetUniformLocation(ID, name.c_str()), vec.x, vec.y, vec.z);
 }
-void Shader::setMat4f(const std::string& name, const float* matValue) const {
+void Shader::setMat4f(const std::string &name, const float *matValue) const
+{
     Use();
     glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE,
                        matValue);
 }
-void Shader::setMat4f(const std::string& name, glm::mat4 mat4) const {
+void Shader::setMat4f(const std::string &name, glm::mat4 mat4) const
+{
     setMat4f(name, glm::value_ptr(mat4));
 }
--- src/OpenGL/VAO.cpp
+++ src/OpenGL/VAO.cpp
@@ -4,10 +4,12 @@
 using namespace std;
 using namespace LUNA;
 
-VAO::VAO(void const* data,
+VAO::VAO(void const *data,
          size_t dataSize,
-         const vector<unsigned int>& attrLen) {
-    if (data == NULL || dataSize == 0 || attrLen.size() == 0) {
+         const vector<unsigned int> &attrLen)
+{
+    if (data == NULL || dataSize == 0 || attrLen.size() == 0)
+    {
         isValid = false;
         ID = 0;
         return;
@@ -19,13 +21,14 @@ VAO::VAO(void const* data,
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, dataSize, data, GL_STATIC_DRAW);
     unsigned int patchLen = 0;
-    for (auto& len : attrLen)
+    for (auto &len : attrLen)
         patchLen += len;
-    for (unsigned int i = 0, cur = 0; i < attrLen.size(); i++) {
+    for (unsigned int i = 0, cur = 0; i < attrLen.size(); i++)
+    {
         glEnableVertexAttribArray(i);
         glVertexAttribPointer(i, attrLen[i], GL_FLOAT, GL_FALSE,
                               patchLen * sizeof(float),
-                              (void*)(cur * sizeof(float)));
+                              (void *)(cur * sizeof(float)));
         cur += attrLen[i];
     }
     glBindVertexArray(0);
@@ -33,25 +36,29 @@ VAO::VAO(void const* data,
     pointNum = dataSize / (sizeof(float) * patchLen);
     hasIndex = false;
     isValid = true;
+    mode = GL_TRIANGLES;
 }
 
-VAO::VAO(void const* data,
+VAO::VAO(void const *data,
          unsigned int dataSize,
-         const std::vector<unsigned int>& attrLen,
-         unsigned int const* index,
+         const std::vector<unsigned int> &attrLen,
+         unsigned int const *index,
          unsigned int indexSize)
-    : VAO(data, dataSize, attrLen) {
+    : VAO(data, dataSize, attrLen)
+{
     isValid = GenBindEBO(index, indexSize);
 }
 
-VAO::~VAO() {
+VAO::~VAO()
+{
     glDeleteVertexArrays(1, &ID);
     if (hasIndex)
         glDeleteBuffers(1, &EBO);
     glDeleteBuffers(1, &VBO);
 }
 
-bool VAO::GenBindEBO(unsigned int const* index, unsigned int indexSize) {
+bool VAO::GenBindEBO(unsigned int const *index, unsigned int indexSize)
+{
     Use();
     if (index == NULL || indexSize <= 0)
         return false;
@@ -63,30 +70,40 @@ bool VAO::GenBindEBO(unsigned int const* index, unsigned int indexSize) {
     return true;
 }
 
-unsigned int VAO::GetID() const {
+unsigned int VAO::GetID() const
+{
     return ID;
 }
-unsigned int VAO::GetAttrNum() const {
+unsigned int VAO::GetAttrNum() const
+{
     return attrNum;
 }
-bool VAO::IsValid() const {
+bool VAO::IsValid() const
+{
     return isValid;
 }
-bool VAO::Use() const {
-    if (!IsValid()) {
+bool VAO::Use() const
+{
+    if (!IsValid())
+    {
         return false;
     }
     glBindVertexArray(ID);
     return true;
 }
-bool VAO::Draw() const {
-    if (!Use()) {
+bool VAO::Draw() const
+{
+    if (!Use())
         return false;
-    }
-    if (hasIndex) {
-        glDrawElements(GL_TRIANGLES, pointNum, GL_UNSIGNED_INT, 0);
-    } else
-        glDrawArrays(GL_TRIANGLES, 0, pointNum);
+
+    if (hasIndex)
+        glDrawElements(mode, pointNum, GL_UNSIGNED_INT, 0);
+    else
+        glDrawArrays(mode, 0, pointNum);
     glBindVertexArray(0);
     return true;
+}
+void VAO::SetRenderMode(unsigned int _mode)
+{
+    mode = _mode;
 }
