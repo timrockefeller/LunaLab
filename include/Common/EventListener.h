ae43991153f25ee93446dac84354d07512e24a42
kitekii <tim720719@163.com>
2021-04-15 15:31:55 +0800

    responce for mouse buttons

--- include/Common/EventListener.h
+++ include/Common/EventListener.h
@@ -8,6 +8,7 @@ class EventListener : public Singleton<EventListener> {
     friend class Singleton<EventListener>;
 
    public:
+
     enum Event_Type {
         // KEY = 0x00000000 ~ 0x0000FFFF
 
@@ -15,9 +16,14 @@ class EventListener : public Singleton<EventListener> {
         KEYBOARD_REPEAT = 0x00020000,
         KEYBOARD_RELEASE = 0x00040000,
         KEYBOARD = KEYBOARD_PRESS | KEYBOARD_REPEAT | KEYBOARD_RELEASE,
+
         MOUSE_SCROLL = 0x00080000,
         MOUSE_MOVE = 0x00080001,
-        WINDOW_ZOOM = 0x00080002,
+
+        MOUSE_PRESS = 0x00080010,
+        MOUSE_RELEASE = 0x00080020,
+        
+        WINDOW_ZOOM = 0x00080100,
     };
 
     EventListener* bind(size_t event, const std::function<void()>& op);
