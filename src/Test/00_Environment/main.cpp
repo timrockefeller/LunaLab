3baaf640774241aa59e5293f7a868a4bfe7a2071
kitekii <tim720719@163.com>
2021-04-27 00:07:51 +0800

    type test

--- src/Test/00_Environment/main.cpp
+++ src/Test/00_Environment/main.cpp
@@ -1,14 +1,62 @@
-#include <Common/Common.h>
-#include <Editor/Glfw.h>
-#include <imgui/imgui.h>
-#include <imgui/imgui_impl_glfw.h>
-#include <imgui/imgui_impl_opengl3.h>
-using namespace KTKR;
-using namespace LUNA;
+#include <iostream>
 using namespace std;
-int main() {
-    if(!Glfw::getInstance()->Init(1920, 1080, "Luna Test"))
-        return 1;
-    Glfw::getInstance()->RunDemo();
+
+template <size_t idx, typename T>
+struct G;
+
+template <typename... T>
+struct V
+{
+};
+
+template <typename T, typename... R>
+struct V<T, R...>
+{
+    V(const T &v, const R &...r)
+        : v(v), r(r...){};
+
+    T v;
+    V<R...> r;
+
+    template <size_t idx>
+    auto get()
+    {
+        return G<idx, V<T, R...>>::get(*this);
+    }
+};
+
+template <typename T, typename... R>
+struct G<0, V<T, R...>>
+{
+    static T get(V<T, R...> &d)
+    {
+        return d.v;
+    }
+};
+
+template <size_t idx, typename T, typename... R>
+struct G<idx, V<T, R...>>
+{
+    static auto get(V<T, R...> &d)
+    {
+        return G<idx - 1, V<R...>>::get(d.r);
+    }
+};
+
+struct Cr
+{
+    int a;
+    float b;
+    double c;
+};
+
+int main()
+{
+    V<int, float, double> c{1, 2, 3};
+    Cr an{2, 3, 4};
+    std::cout << c.get<0>();
+    std::cout << an.a;
+    std::cout << sizeof(c) << std::endl;
+    std::cout << sizeof(an) << std::endl;
     return 0;
 }
