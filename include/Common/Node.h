14a585b9138d0378d299b6ec6e423204f06eddf5
kitekii <tim720719@163.com>
2021-04-29 20:12:34 +0800

    fix link bug

--- include/Common/Node.h
+++ include/Common/Node.h
@@ -13,12 +13,38 @@ namespace KTKR
         virtual ~Node() = default;
 
     public:
-        void AddChild(Ptr<ImplT> _child);
-        void DelChild(Ptr<ImplT> _child);
+        void AddChild(Ptr<ImplT> _child)
+        {
+            // remove lagency parent
+            if (!_child->parent.expired())
+                _child->parent.lock()->DelChild(_child);
+            ;
+
+            _child->parent = This<ImplT>();
+            children.insert(_child);
+        }
+        void DelChild(Ptr<ImplT> _child)
+        {
+            if (_child->parent.lock() == This())
+            {
+                children.erase(_child);
+                _child->parent.reset();
+            }
+        }
 
     public:
-        bool IsDescendantOf(Ptr<ImplT> node);
-        bool IsAncentorOf(Ptr<ImplT> node);
+        bool IsDescendantOf(Ptr<ImplT> node)
+        {
+            if (node == This<ImplT>())
+                return true;
+            if (parent->expired())
+                return false;
+            return parent->lock()->IsDescendantOf(node);
+        }
+        bool IsAncentorOf(Ptr<ImplT> node)
+        {
+            return node->IsDescendantOf(This<ImplT>());
+        }
 
     public:
         const Ptr<ImplT> GetParent() const { return parent.lock(); }
