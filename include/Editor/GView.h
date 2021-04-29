3893edee8aba120765edaa7604c03a4533e62992
kitekii <tim720719@163.com>
2021-04-29 20:12:45 +0800

    + GView

---
+++ include/Editor/GView.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <Common/Common.h>
+
+#include "Scene/Scene.h"
+namespace LUNA::Editor
+{
+	class GView : public KTKR::Singleton<GView>
+	{
+
+
+	public:
+
+		GView();
+
+		KTKR::Ptr<Scene> scene;
+
+		KTKR::Ptr<SObj> GetSceneRoot() { return scene->GetRoot(); }
+	};
+}
