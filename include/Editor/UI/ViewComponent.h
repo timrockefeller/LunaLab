807c04a803dceb61a77550bca2c7806183d21e1f
kitekii <tim720719@163.com>
2021-04-29 20:13:59 +0800

    style

--- include/Editor/UI/ViewComponent.h
+++ include/Editor/UI/ViewComponent.h
@@ -9,19 +9,21 @@
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_opengl3.h>
 
-namespace LUNA::Editor {
+namespace LUNA::Editor
+{
 
-class ViewComponent {
-   public:
-    ViewComponent() = default;
-    virtual ~ViewComponent() = default;
-    short instanceID = 0xFF;
+    class ViewComponent
+    {
+    public:
+        ViewComponent() = default;
+        virtual ~ViewComponent() = default;
+        size_t instanceID = 0xFF;
+        bool enabled = true;
+        // implement these function in every UI module.
 
-    // implement these function in every UI module.
+        virtual bool Enter() = 0;
+        virtual bool Draw() = 0;
+        virtual bool Close() = 0;
+    };
 
-    virtual bool Enter() const = 0;
-    virtual bool Draw() const = 0;
-    virtual bool Close() const = 0;
-};
-
-}  // namespace LUNA::Editor
+} // namespace LUNA::Editor
